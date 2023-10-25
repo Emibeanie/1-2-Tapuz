b4fb80152f319c938c19fbdd71d3a2568ed8fea0
Emibeanie <emily.gendelev@gmail.com>
2023-10-25 20:33:24 +0300

    fixed jump + added camera movement

--- 0.5 Tapuz/Assets/Scripts/playerController.cs
+++ 0.5 Tapuz/Assets/Scripts/playerController.cs
@@ -7,7 +7,20 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] SpriteRenderer sr;
+
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public float jumpStartTime;
     
+    private float moveInput;
+    private float jumpTime;
+    private bool isGrounded;
+    private bool isJumping;
+
     void Start()
     {
         
@@ -16,14 +29,50 @@ public class playerController : MonoBehaviour
    
     void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxisRaw("Horizontal");
+        FaceMoveDirection();
+        Jump();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+    void FaceMoveDirection()
+    {
+        if (moveInput > 0)
+            sr.flipX = false;
+        else if (moveInput < 0)
+            sr.flipX = true;
+    }
+    
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-       
-        if (Input.GetKey("space"))
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(0,7f);
+            isJumping = true;
+            jumpTime = jumpStartTime;
+            rb.velocity = Vector2.up * jumpForce;
         }
 
+        if (Input.GetButton("Jump") && isJumping == true)
+        {
+            if (jumpTime > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTime -= Time.deltaTime;
+            }
+            else
+                isJumping = false;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+            isJumping = false;
+    }
+
+    void OnDirection()
+    {
+        Gizmos.DrawWireSphere(feetPos.position, checkRadius);
     }
 }
