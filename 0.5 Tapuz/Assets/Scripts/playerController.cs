db4cecc10d8cb747fdc96becea8bab5118b0f068
Emibeanie <emily.gendelev@gmail.com>
2023-10-30 22:07:33 +0200

    working on animations

--- 0.5 Tapuz/Assets/Scripts/playerController.cs
+++ 0.5 Tapuz/Assets/Scripts/playerController.cs
@@ -7,33 +7,24 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
 
     public float moveSpeed;
     public float jumpForce;
-    public Transform feetPos;
-    public float checkRadius;
-    public LayerMask whatIsGround;
     public float jumpStartTime;
+    public float checkRadius;
+    public Transform feetPos;
+    public LayerMask whatIsGround;
     
     private float moveInput;
     private float jumpTime;
     private bool isGrounded;
     private bool isJumping;
     private bool isFacingRight = true;
-   
+    
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        if(moveInput > 0 && !isFacingRight)
-        {
-            isFacingRight = true;
-            FaceMoveDirection();
-        }
-        else if(moveInput < 0 && isFacingRight)
-        {
-            isFacingRight = false;
-            FaceMoveDirection();
-        }
+        Movement();
         FaceMoveDirection();
         Jump();
     }
@@ -41,36 +32,75 @@ public class playerController : MonoBehaviour
     {
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
+    void Movement()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (moveInput != 0)
+        {
+            animator.SetBool("IsRunning", true);
+            animator.SetBool("IsIdle", false);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+            animator.SetBool("IsIdle", true);
+        }
+
+        if (moveInput > 0 && !isFacingRight)
+        {
+            isFacingRight = true;
+            FaceMoveDirection();
+        }
+        else if (moveInput < 0 && isFacingRight)
+        {
+            isFacingRight = false;
+            FaceMoveDirection();
+        }
+    }
     void FaceMoveDirection()
     {
         bool flipped = !isFacingRight;
-        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+        transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
     }
     
     void Jump()
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-
+       
+        //short jump
         if (isGrounded == true && Input.GetButtonDown("Jump"))
         {
             isJumping = true;
             jumpTime = jumpStartTime;
             rb.velocity = Vector2.up * jumpForce;
+            animator.SetBool("IsJumpingL", true);
         }
-
+        
+        //long jump
         if (Input.GetButton("Jump") && isJumping == true)
         {
             if (jumpTime > 0)
             {
                 rb.velocity = Vector2.up * jumpForce;
                 jumpTime -= Time.deltaTime;
+                animator.SetBool("IsJumpingH", true);
             }
             else
+            {
                 isJumping = false;
+                animator.SetBool("IsJumpingH", false);
+                animator.SetBool("IsJumpingL", false);
+            }
         }
 
         if (Input.GetButtonUp("Jump"))
+        {
             isJumping = false;
+            animator.SetBool("IsJumpingH", false);
+            animator.SetBool("IsJumpingL", false);
+            animator.SetBool("IsIdle", true);
+        }
     }
 
     void OnDirection()
