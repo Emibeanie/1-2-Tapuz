618b52531babb4af7c720afed05ae7992ebc26f1
Emibeanie <emily.gendelev@gmail.com>
2023-11-13 21:13:06 +0200

    working on jumping

--- 0.5 Tapuz/Assets/Scripts/playerController.cs
+++ 0.5 Tapuz/Assets/Scripts/playerController.cs
@@ -14,17 +14,16 @@ public class playerController : MonoBehaviour
 
     [Header("Physics")]
     public float moveSpeed;
-    public float shortJumpForce;
-    public float longJumpForce;
-    public float maxJumpTime;
-    public float jumpStartTime;
+    public float jumpForce;
     public float checkRadius = 2f;
     private float moveInput;
 
-    private float jumpTime;
+    private float jumpPressedRememberTime = 0.2f;
+    private float jumpPressedRemember = 0;
+    private float groundedRemember = 0;
+    public float groundedRememberTime;
+    public float cutJumpHeight;
     private bool isGrounded;
-    private bool isJumping;
-    private bool isJumpButtonDown;
     private bool isFacingRight = true;
 
     void Update()
@@ -50,8 +49,11 @@ public class playerController : MonoBehaviour
         }
         else
         {
-            animator.SetBool("IsRunning", false);
-            animator.SetBool("IsIdle", true);
+            if (isGrounded)
+            {
+                animator.SetBool("IsRunning", false);
+                animator.SetBool("IsIdle", true);
+            }
         }
 
         if (moveInput > 0 && !isFacingRight)
@@ -76,64 +78,114 @@ public class playerController : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
         animator.SetBool("IsGrounded", isGrounded);
 
-        if (isJumping)
+        groundedRemember -= Time.deltaTime;
+        if (isGrounded)
         {
-            if (rb.velocity.y < 0)
-            {
-                isJumping = false;
-                animator.SetBool("IsJumping", false);
-            }
-        }
-        else
-        {
-            if (isGrounded)
-            {
-                animator.SetBool("IsJumping", false);
-
-
-                if (Input.GetButtonDown("Jump"))
-                {
-                    isJumping = true;
-                    jumpTime = 0.0f;
-                    isJumpButtonDown = true;
-
-                    animator.SetBool("IsRunning", false);
-                    animator.SetBool("IsIdle", false);
-                    animator.SetBool("IsJumping", true);
-
-                    rb.velocity = Vector2.up * shortJumpForce;
-                }
-            }
+            groundedRemember = groundedRememberTime;
         }
 
-        if (Input.GetButton("Jump") && isJumping)
+        jumpPressedRemember -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
         {
-            if (isJumpButtonDown)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, shortJumpForce);
-                isJumpButtonDown = false;
-            }
-            if (jumpTime < maxJumpTime)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, shortJumpForce);
-                jumpTime += Time.deltaTime;
-            }
-            else
-            {
-                rb.velocity = new Vector2(rb.velocity.x, longJumpForce);
-            }
+            jumpPressedRemember = jumpPressedRememberTime;
         }
 
+        if(isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                animator.SetBool("IsJumping", true);
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
 
         if (Input.GetButtonUp("Jump"))
         {
-            isJumping = false;
-            isJumpButtonDown = false;
-
-            animator.SetBool("IsRunning", false);
-            animator.SetBool("IsIdle", true);
             animator.SetBool("IsJumping", false);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Ground"))
+        {
+            Debug.Log("DETECTEDDDDDD");
+            animator.SetBool("IsJumping", false);
+            isGrounded = true;
+        }
+    }
 }
+
+
+
+//void Jump()
+//{
+//    isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+//    animator.SetBool("IsGrounded", isGrounded);
+//    fGroundedRemember -= Time.deltaTime;
+//    fJumpPressedRemember -= Time.deltaTime;
+
+//    if (isJumping)
+//    {
+//        if (rb.velocity.y < 0)
+//        {
+//            isJumping = false;
+//            animator.SetBool("IsJumping", false);
+//        }
+//    }
+//    else
+//    {
+//        if (isGrounded)
+//        {
+//            animator.SetBool("IsJumping", false);
+//            fGroundedRemember = fGroundedRememberTime;
+
+
+//            if (Input.GetButtonDown("Jump"))
+//            {
+//                isJumping = true;
+//                jumpTime = 0.0f;
+//                isJumpButtonDown = true;
+//                fJumpPressedRemember = fJumpPressedRememberTime;
+
+//                animator.SetBool("IsRunning", false);
+//                animator.SetBool("IsIdle", false);
+//                animator.SetBool("IsJumping", true);
+
+//                rb.velocity = Vector2.up * shortJumpForce;
+//            }
+//        }
+//    }
+
+//    if (Input.GetButton("Jump") && isJumping)
+//    {
+//        if (isJumpButtonDown)
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, shortJumpForce);
+//            isJumpButtonDown = false;
+//        }
+//        if (jumpTime < maxJumpTime)
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, shortJumpForce);
+//            jumpTime += Time.deltaTime;
+//        }
+//        else
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, longJumpForce);
+//        }
+//    }
+
+
+//    if (Input.GetButtonUp("Jump"))
+//    {
+//        isJumping = false;
+//        isJumpButtonDown = false;
+
+//        animator.SetBool("IsRunning", false);
+//        animator.SetBool("IsIdle", true);
+//        animator.SetBool("IsJumping", false);
+//    }
+//}
+//}
+
 
