55bf321899466cf78155bef21c4c4c025ce098e5
Emibeanie <emily.gendelev@gmail.com>
2023-11-25 21:26:30 +0200

    working on chained plat

--- 0.5 Tapuz/Assets/Scripts/chainedPlat.cs
+++ 0.5 Tapuz/Assets/Scripts/chainedPlat.cs
@@ -5,48 +5,59 @@ using UnityEngine;
 public class chainedPlat : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
+    [SerializeField] Animator anim;
 
-    private Vector2 originalPosition;
-    public float moveDistance;
-    public float moveSpeed;
+    //private Vector2 originalPosition;
+    //public float moveDistance;
+    //public float moveSpeed;
 
-    private void Start()
-    {
-        originalPosition = transform.position;
-    }
+    //private void Start()
+    //{
+    //    originalPosition = transform.position;
+    //}
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            
+            anim.SetBool("Move", true);
             audioSource.Play();
-            StartCoroutine(MovePlatform());
+
+            //StartCoroutine(MovePlatform());
         }
     }
-    private IEnumerator MovePlatform()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        // Move down
-        float t = 0f;
-        Vector2 targetPosition = originalPosition - Vector2.up * moveDistance;
-
-        while (t < 0.5f)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            t += Time.deltaTime * moveSpeed;
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, t);
-            yield return null;
+            anim.SetBool("Move", false);
         }
-
-        yield return new WaitForSeconds(0.2f);
-
-        //move back up
-        t = 0f;
-        while (t < 0.5f)
-        {
-            t += Time.deltaTime * moveSpeed;
-            transform.position = Vector2.Lerp(targetPosition, originalPosition, t);
-            yield return null;
-        }
-        // Ensure the platform is back at the original position
-        transform.position = originalPosition;
     }
+    //private IEnumerator MovePlatform()
+    //{
+    //    // Move down
+    //    float t = 0f;
+    //    Vector2 targetPosition = originalPosition - Vector2.up * moveDistance;
+
+    //    while (t < 0.2f)
+    //    {
+    //        t += Time.deltaTime * moveSpeed;
+    //        transform.position = Vector3.Lerp(originalPosition, targetPosition, t);
+    //        yield return null;
+    //    }
+
+    //    yield return new WaitForSeconds(0.2f);
+
+    //    //move back up
+    //    t = 0f;
+    //    while (t < 0.2f)
+    //    {
+    //        t += Time.deltaTime * moveSpeed;
+    //        transform.position = Vector2.Lerp(targetPosition, originalPosition, t);
+    //        yield return null;
+    //    }
+    //    // Ensure the platform is back at the original position
+    //    transform.position = originalPosition;
+    //}
 }
