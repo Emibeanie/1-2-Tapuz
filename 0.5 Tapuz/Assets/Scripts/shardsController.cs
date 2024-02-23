580da22d0540b6c1f7b587eb6a10e948ee09d49c
Emibeanie <emily.gendelev@gmail.com>
2024-02-24 00:03:31 +0200

    added shard anim+parti for trailer

--- 0.5 Tapuz/Assets/Scripts/shardsController.cs
+++ 0.5 Tapuz/Assets/Scripts/shardsController.cs
@@ -11,29 +11,49 @@ public class shardsController : MonoBehaviour
     //[SerializeField] AudioSource audioSource;
     //[SerializeField] AudioClip startSound;
     //[SerializeField] AudioClip loopSound;
+    [SerializeField] Animator anim;//
+    [SerializeField] GameObject particles;//
+    Collider2D m_Collider;//
+
 
     private void Start()
     {
         //StartCoroutine(shardSound());
+        anim.SetBool("fly", false);//
+        m_Collider = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
         if (ShardPanel.activeSelf)
             if (Input.GetKeyDown(KeyCode.Escape))
+            {
                 ExitButton();
+                anim.SetBool("fly", true);
+                particles.SetActive(true);
+            }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-            ShardPanel.SetActive(true);
+        {
+            parti();
+            //ShardPanel.SetActive(true);
+        }
     }
 
+    public void parti()
+    {
+        anim.SetBool("fly", true);
+        particles.SetActive(true);
+        m_Collider.enabled = false;
+    }
     public void ExitButton()
     {
         ShardPanel.SetActive(false);
-        ShardSprite.SetActive(false);
-        SymbolSprite.SetActive(true); 
+        //ShardSprite.SetActive(false);
+        //SymbolSprite.SetActive(true); 
         LockShardActive.SetActive(true);
     }
     //IEnumerator shardSound()
